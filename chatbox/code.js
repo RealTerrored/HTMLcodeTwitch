ebf1ac65106a4a97fd56e8c0c7251d3b23ab8b95
RealTerrored <patrikko.onddo@gmail.com>
2023-07-15 10:58:00 +0200

    modified:   chatbox/code.js 	modified:   chatbox/style.css

--- chatbox/code.js
+++ chatbox/code.js
@@ -2,13 +2,33 @@
 ////////////////Twitch Chatbox by TerroredUnleashed/////////////////
 ////////////////////////////////////////////////////////////////////
 n = 5 ///number of seconds to display on scene, 0 means infinite////
-f = 3 //fade duration in seconds////////////////////////////////////
+f = 3 ///fade duration in seconds///////////////////////////////////
 
 function Overflow(node) {
 	return node.scrollHeight > node.clientHeight
 }
-
-
+function chartest(str) {
+	const specialchar = /[|]/
+	return specialchar.test(str)
+}
+function typewriter(element, message, name) {
+	let z = 0
+	 let typer = setInterval(() => {
+		if (z < message.length) {
+			if (z <= name.length){
+					element.innerHTML = "<strong>" + element.innerHTML.replace('<strong>','').replace('</strong>','') + message[z] + "</strong>"
+				}
+			else {
+					element.innerHTML = element.innerHTML + message[z]
+				}
+			z++
+		}
+		else {
+			//element.innerHTML = message
+			clearInterval(typer)
+		}
+	}, 25)
+}
 
 let output = document.getElementById("chatbox"), childID;
 const client = new tmi.Client({
@@ -24,7 +44,7 @@ client.on('message', (channel, tags, message, self) => {
 	window["element" + x].classList.add("messages")
 	window["element" + x].id = x
 	output.appendChild(window["element" + x])
-	window["element" + x].innerHTML = `${tags['display-name']}: ${message}`
+	typewriter(window["element" + x], `${tags['display-name']}: ${message}`, tags['display-name'])
 	x++
 });
 if (n != 0) {
@@ -41,7 +61,7 @@ if (n != 0) {
 						window["temp" + childID.id]--
 					}
 					if (window["temp" + childID.id] == 0) {
-						childID.style.animation = `fade ${f}s linear`
+						childID.style.animation = `fade-out ${f}s linear`
 						childID.addEventListener("animationend", function() {
 							this.remove()
 						});
@@ -59,7 +79,7 @@ client.on('message', (channel, tags, message, self) => {
 	for (i=1; i < output.childNodes.length; i++) {
 		childElement = output.childNodes[i]
 		if (childElement.getBoundingClientRect().top < output.getBoundingClientRect().top){
-			childElement.style.animation = `fade ${f}s linear`
+			childElement.style.animation = `fade-out ${f}s linear`
 			childElement.addEventListener("animationend", function() {
 				this.remove()
 			});
